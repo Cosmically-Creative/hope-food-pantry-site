da2a1f951926242d938a4297de5d99637f162390
blake <blake@boonefamily.net>
2025-11-20 08:51:17 -0600

    Added scroll animations for all pages

--- script.js
+++ script.js
@@ -33,3 +33,21 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+// Scroll Animations
+document.addEventListener("DOMContentLoaded", () => {
+  const observerOptions = { root: null, rootMargin: "0px", threshold: 0.75 };
+  const onIntersection = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  };
+  const observer = new IntersectionObserver(onIntersection, observerOptions);
+  const elementsToAnimate = document.querySelectorAll(
+    ".animate-on-scroll, .animate-on-scroll-delay"
+  );
+  elementsToAnimate.forEach((el) => observer.observe(el));
+});
